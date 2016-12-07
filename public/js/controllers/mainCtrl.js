37d9a72a1df49593ac8f9622905589961291d943
ajwehunt <adamjwehunt@gmail.com>
2016-12-07 13:31:28 -0700

    google analytics and meta tags

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -83,3 +83,20 @@ function scrollTo(element) {
   }
   $("html, body").animate({ scrollTop: position }, "slow");
 }
+
+(function() {
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.async = true;
+s.src = '//cdn.youracclaim.com/assets/utilities/embed.js';
+var o = document.getElementsByTagName('script')[0];
+o.parentNode.insertBefore(s, o);
+})();
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-88638593-1', 'auto');
+ga('send', 'pageview');
